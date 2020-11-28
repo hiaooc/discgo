913e8748d35f2fa171880b199c0bc016977c31b5
Markus Cisler <hello@markuscisler.com>
2020-11-28 16:52:20 +0100

    Remove http server

--- cmd/bot/main.go
+++ cmd/bot/main.go
@@ -4,22 +4,18 @@ import (
 	"flag"
 	"fmt"
 	"log"
-	ghttp "net/http"
 	"os"
 	"os/signal"
 	"syscall"
 
 	"github.com/bwmarrin/discordgo"
-	"github.com/go-chi/chi"
 	"github.com/hiaooc/discgo/pkg/datastore"
 	"github.com/hiaooc/discgo/pkg/handler"
-	"github.com/hiaooc/discgo/pkg/http"
 	"github.com/hiaooc/discgo/pkg/slackbot"
 )
 
 var (
 	dataStorePath = flag.String("datastore", "", "Path to JSON file")
-	addr          = flag.String("listen", ":8080", "Listen address")
 )
 
 func main() {
@@ -33,45 +29,31 @@ func main() {
 		log.Fatal("-datastore must not be empty")
 	}
 
-	discord, err := discordgo.New("Bot " + token)
-	if err != nil {
-		log.Fatal(err)
-	}
-
 	ds, err := datastore.Read(*dataStorePath)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	bot := slackbot.New(ds)
-	discord.AddHandler(bot.Handler)
-	discord.AddHandler(handler.ChangeTopic)
-
-	discord.AddHandler(handler.PinMessage)
-	discord.AddHandler(handler.UnpinMessage)
-
-	err = discord.Open()
+	dg, err := discordgo.New("Bot " + token)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	handler := http.NewHandler(ds)
-	router := chi.NewRouter()
-	router.Get("/config", handler.ReadConfig)
-	router.Post("/config", handler.WriteConfig)
-	srv := ghttp.Server{
-		Addr:    *addr,
-		Handler: router,
+	bot := slackbot.New(ds)
+	dg.AddHandler(bot.Handler)
+
+	dg.AddHandler(handler.ChangeTopic)
+	dg.AddHandler(handler.PinMessage)
+	dg.AddHandler(handler.UnpinMessage)
+
+	err = dg.Open()
+	if err != nil {
+		log.Fatal(err)
 	}
 
+	fmt.Println("Bot is now running.  Press CTRL-C to exit.")
 	sc := make(chan os.Signal, 1)
 	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt)
-	go func() {
-		<-sc
-		discord.Close()
-		srv.Close()
-	}()
-
-	fmt.Println("Bot is now running.  Press CTRL-C to exit.")
-	log.Fatal(srv.ListenAndServe())
+	<-sc
+	dg.Close()
 }
--- go.mod
+++ go.mod
@@ -2,7 +2,4 @@ module github.com/hiaooc/discgo
 
 go 1.15
 
-require (
-	github.com/bwmarrin/discordgo v0.22.0
-	github.com/go-chi/chi v1.5.0
-)
+require github.com/bwmarrin/discordgo v0.22.0
